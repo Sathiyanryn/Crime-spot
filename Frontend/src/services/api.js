9af322b3a4fcbc1113649e4e3b20dbbb5b266b01
Sathiyanarayanan <narayanansathiya2005@gmail.com>
2026-03-23 00:11:53 +0530

    Updated project files

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken, logout } from './auth';
 
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:5000';
+  import.meta.env.VITE_API_BASE_URL || 'http://3.87.167.103:5000';
 
 const API = axios.create({
   baseURL: API_BASE_URL,
